2342072b04ec6588317a468b5da6afc4f02aeaa3
su-xingyu <suxingyu98@gmail.com>
2024-05-23 20:11:54 +0200

    more examples

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 ./gradlew run --args="-e optimizeString -o class --doop-dir /home/xinsu/Desktop/Thesis/doop --optimize --keep-tmp-dir"
 #./gradlew run --args="-e optimizeVar -o class --doop-dir /home/xinsu/Desktop/Thesis/doop --optimize --keep-tmp-dir"
+#./gradlew run --args="-e optimizeNotConsecutive -o class --doop-dir /home/xinsu/Desktop/Thesis/doop --optimize --keep-tmp-dir"
 #./gradlew run --args="-e inlineA -o class --doop-dir /home/xinsu/Desktop/Thesis/doop --inline --optimize --keep-tmp-dir"
 #./gradlew run --args="-e inlineB -o class --doop-dir /home/xinsu/Desktop/Thesis/doop --inline --optimize --keep-tmp-dir"
 #./gradlew run --args="-e inlineC -o class --doop-dir /home/xinsu/Desktop/Thesis/doop --inline --optimize --keep-tmp-dir"
-
+#./gradlew run --args="-e inlineD -o class --doop-dir /home/xinsu/Desktop/Thesis/doop --inline --optimize --keep-tmp-dir"
